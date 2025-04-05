41993dcce87bab88a4e1c91d900cf3395682de54
Mohamed Salah <164447928+xXSALAHELEQASXx@users.noreply.github.com>
2025-04-05 09:27:47 +0300

    Add files via upload

---
+++ Task-1(JS)/script.js
@@ -0,0 +1,49 @@
+/* Task1
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);//15
+console.log(num1 - num2);//5
+console.log(num1 * num2);//50
+console.log(num1 / num2);//2
+console.log(num1 % num2);//0
+console.log(num1++);//10
+console.log(++num1);//12 */
+
+//---------------------------------------------------------------------
+/* Task 2
+
+let age = 17;
+if (age === 20){
+    console.log("You are in the third grade");
+}
+else if (age > 18){
+    console.log(`Your age is: ${age}`);
+}
+else{
+    console.log("This age is not valid");
+} */
+
+//---------------------------------------------------------------------
+/*Task3
+ let grade = "B+";
+switch (grade) {
+    case "A":
+    case "A+":
+    case "A-":
+        console.log("Excellent!");
+        break;
+    case "B":
+    case "B+":
+    case "B-":
+        console.log("Very Good!");
+        break;
+    case "C":
+    case "C+":
+    case "C-":
+        console.log("Good");
+        break;
+    default:
+        console.log("Invalid grade");
+        break;
+} */
